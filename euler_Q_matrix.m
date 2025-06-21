f3f61d0717c5b4a173594daa0ecb15d62acfdf32
dvideira14 <diogovideira07@gmail.com>
2025-06-21 19:10:18 +0100

    Create euler_Q_matrix.m

---
+++ euler_Q_matrix.m
@@ -0,0 +1,5 @@
+function Q = euler_Q_matrix(phi, theta)
+    Q = [1, sin(phi)*tan(theta), cos(phi)*tan(theta);
+         0, cos(phi),           -sin(phi);
+         0, sin(phi)/cos(theta), cos(phi)/cos(theta)];
+end
